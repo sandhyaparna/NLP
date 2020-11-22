2c5e0820b5231bb1acc95612fa2dbb3af366c6bc
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2020-11-21 21:26:20 -0600

    Update 01_Preprocessing_Visualization.py

--- 01_Preprocessing_Visualization.py
+++ 01_Preprocessing_Visualization.py
@@ -161,7 +161,7 @@ result
 Output:
 ['Analyt', 'cs Vidhya']
 
-# re.sub(pattern, repl, string): It helps to search a pattern and replace with a new sub string. If the pattern is not found, string is returned unchanged.
+# re.sub(pattern, repl, string): substitute. It helps to search a pattern and replace with a new sub string. If the pattern is not found, string is returned unchanged.
 result=re.sub(r'India','the World','AV is largest Analytics community of India')
 result
 Output:
